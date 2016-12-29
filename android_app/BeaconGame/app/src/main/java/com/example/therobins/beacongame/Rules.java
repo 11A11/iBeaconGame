3ac6f3db26633333e8f69f5f01ae83d6c2badd95
SHIKHA DAHIYA <shikhadahiya1997@gmail.com>
2016-12-29 16:15:01 +0530

    Rules updated

--- android_app/BeaconGame/app/src/main/java/com/example/therobins/beacongame/Rules.java
+++ android_app/BeaconGame/app/src/main/java/com/example/therobins/beacongame/Rules.java
@@ -14,6 +14,9 @@ public class Rules extends AppCompatActivity {
         setContentView(R.layout.activity_rules);
 
         text1 = (TextView)findViewById(R.id.textView1);
-        text1.setText("Rules : \n 1. Something you need to do" + "\n 2. Another rule");
+        text1.setText("Rules/Format : \n 1. 5 beacons have been placed around the campus\n" +
+                "\n 2. When you reach at a beacon, the hint would be played on your phone\n" +
+                "\n 3. Use this hint to get to the next level, where you would encounter another beacon\n"
+        + "\n 4. You can access the hints at any level if and only if all the previous levels have been unlocked\n");
     }
 }
